0b4622025a67b2a9aec5cf5bb624a2cd36a2c11a
mh4nx7net <androxoss@hotmail.com>
2022-11-06 15:47:05 +0700

    readable python code

--- belajar.py
+++ belajar.py
@@ -1,38 +1,61 @@
-Nama = {}
+user = {
+    'nama': None,
+    'nim': None,
+    'buku': []
+}
 
-NIM = {}
 
-BukuPinjam = {}
 def menu():
-    x = str(input('meminjam buku y/n = '))
-    if x == 'y':
-        nama()
-        ulang()
-    else:
-        menu()
+    while True:
+        print('meminjam buku => yes/no')
+        print('1. yes untuk meminjam buku')
+        print('2. no untuk keluar')
+        x = str(input('> '))
+        if x == 'yes' or x == 'y':
+            simpanNama()
+            simpanNim()
+            simpanBuku()
+            rincian()
+        elif x == 'no' or x == 'n':
+            break
+        else:
+            print('Perintah tidak dikenal')
 
-def nama():
-    Nama['Pelanggan 2'] = str(input('Masukkan nama = '))
-    print('Nama Anda Telah Diinput')
-    nim()
 
-def nim():
-    NIM['Pelanggan 2'] =int(input('Masukkan NIM = '))
-    print('NIM Anda Telah Diinput')
-    buku()
-def buku():
-    While buku1 in buku:
-              print (buku1)
-              
-              
-def ulang():
+def simpanNama():
+    user['nama'] = str(input('\nMasukkan nama = '))
+    print('Nama Anda telah diinput')
 
-    print(' ')
-    print('======================================')
+
+def simpanNim():
+    user['nim'] = int(input('Masukkan nim = '))
+    print('NIM Anda telah diinput')
+
+
+def simpanBuku():
+    while True:
+        Buku = str(input('Masukan buku = '))
+        user["buku"].append(Buku)
+
+        action = str(input('\ntambah buku ? '))
+        if (action == 'y' or action == 'yes'):
+            continue
+        elif (action == 'n' or action == 'no'):
+            break
+        else:
+            print('Perintah tidak dikenal')
+    print('Buku Anda telah diinput')
+
+
+def rincian():
+    print('\n======================================')
     print('Rincian Peminjaman Buku Anda :')
-    print('Nama        :', Nama['Pelanggan 2'])
-    print('Nim         :', NIM['Pelanggan 2'])
-    print('Judul Buku  :', BukuPinjam['Pelanggan 2'], BukuPinjam['Pelanggan 3'], BukuPinjam['Pelanggan 2'] )
-    print('Selesaii')
+    print(f'Nama\t\t: {user["nama"]}')
+    print(f'Nim\t\t: {user["nim"]}')
+    print('Buku\t\t:')
+    for index, buku in enumerate(user["buku"], start=1):
+        print(f'\t\t{index}. {buku}')
+    print('Selesaii\n')
+
 
 menu()
